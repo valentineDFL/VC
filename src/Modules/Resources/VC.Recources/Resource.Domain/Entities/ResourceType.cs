2d4dc0902d75c902b2983729ffb295313640cfd9
mwah <annenkovgleb@proton.me>
2025-03-14 20:13:43 +0300

    #VC-8 [feature] Изменяет логику домена

---
+++ src/Modules/Resources/VC.Recources/Resource.Domain/Entities/ResourceType.cs
@@ -0,0 +1,26 @@
+﻿using VC.Recources.Resource.Domain.Exceptions;
+using VC.Recources.Resource.Domain.ValueObject;
+
+namespace VC.Recources.Resource.Domain.Entities;
+
+public class ResourceType
+{
+    public string Name { get; set; }
+
+    public IReadOnlyCollection<AttributeDefinition> ResourceAttributeDefinitions => _resourceAttributeDefinitions.AsReadOnly();
+
+    private List<AttributeDefinition> _resourceAttributeDefinitions = new List<AttributeDefinition>();
+
+    public ResourceType(string name)
+    {
+        Name = name;
+    }
+
+    public void AddAttributeDefinition(AttributeDefinition resourceAttributeDefinitions)
+    {
+        if (_resourceAttributeDefinitions.Any(d => d.Key == resourceAttributeDefinitions.Key))
+            throw new DomainException($"Атрибут {resourceAttributeDefinitions.Key} уже существует.");
+
+        _resourceAttributeDefinitions.Add(resourceAttributeDefinitions);
+    }
+}
---
+++ src/Modules/Resources/VC.Recources/Resource.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿namespace VC.Recources.Resource.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException() { }
+
+    public DomainException(string message)
+        : base(message) { }
+
+    public DomainException(string message, Exception exception)
+        : base(message, exception) { }
+}
