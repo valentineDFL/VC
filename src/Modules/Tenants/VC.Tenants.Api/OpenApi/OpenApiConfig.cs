2e49465bada61151cfa51b04d4361d7532df9c86
valentineDFL <titan46354@gmail.com>
2025-03-16 21:41:50 +0200

    #VC-14 [feature] Добавляет заполнение полей тенанта в Scalar значениями по умолчанию

--- src/Modules/Tenants/VC.Tenants.Api/OpenApi/OpenApiConfig.cs
+++ src/Modules/Tenants/VC.Tenants.Api/OpenApi/OpenApiConfig.cs
@@ -17,10 +17,29 @@ public class OpenApiConfig
                     Version = "v1",
                     Title = "Управление арендаторами"
                 };
-            
+
                 return Task.CompletedTask;
             }
         );
+
+        opts.AddSchemaTransformer((schema, ctx, ctk) =>
+        {
+            foreach (var property in schema.Properties)
+            {
+                var propertySchema = property.Value;
+
+                if (propertySchema.Type == typeof(int).Name.ToLower())
+                    propertySchema.Default = new Microsoft.OpenApi.Any.OpenApiInteger(0);
+
+                if (propertySchema.Type == typeof(string).Name.ToLower())
+                    propertySchema.Default = new Microsoft.OpenApi.Any.OpenApiString("string");
+
+                if (propertySchema.Type == typeof(DateTime).Name.ToLower())
+                    propertySchema.Default = new Microsoft.OpenApi.Any.OpenApiDateTime(DateTime.UtcNow);
+            }
+
+            return Task.CompletedTask;
+        });
 
         return opts;
     }
