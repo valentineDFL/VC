a65ac7dd09718dcb3c6802f535ac9d8d4a13ee19
Воробьев Максим Сергеевич <maflend.job.mp@gmail.com>
2025-05-14 23:27:54 +0300

    Дорабатывает фронт

--- srcui/VC.WebUi/Program.cs
+++ srcui/VC.WebUi/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddScoped(sp => 
     new HttpClient { BaseAddress = new Uri("http://localhost:5056") });
 builder.Services.AddScoped<ApiService>();
