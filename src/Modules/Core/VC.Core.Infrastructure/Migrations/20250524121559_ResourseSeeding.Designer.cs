4f3d89c3e483af2b60c473c2d865f6c5a790f040
alonesampler <vlad20011810@gmail.com>
2025-05-24 18:26:39 +0600

    Реализует сидер ресурсов

--- src/Modules/Core/VC.Core.Infrastructure/Migrations/20250524121559_ResourseSeeding.Designer.cs
+++ src/Modules/Core/VC.Core.Infrastructure/Migrations/20250524121559_ResourseSeeding.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VC.Core.Employees;
@@ -10,12 +11,14 @@ using VC.Core.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace VC.Core.Infrastructure.Persistence.Migrations
+namespace VC.Core.Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250524121559_ResourseSeeding")]
+    partial class ResourseSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
